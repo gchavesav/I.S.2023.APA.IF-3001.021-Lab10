0c06523d824221f786b271c10fb18b2c7e724d62
Sebasms16 <82125411+Sebasms16@users.noreply.github.com>
2023-05-31 20:11:50 -0600

    Test AVL y metodo isBalanced

--- src/test/java/domain/AVLTest.java
+++ src/test/java/domain/AVLTest.java
@@ -10,14 +10,49 @@ class AVLTest {
     void test() {
         try {
             AVL avl = new AVL();
-            for (int i = 0; i < 20; i++) {
-                avl.add(util.Utility.random(99));
-            }
-            System.out.println(avl);
-            System.out.println("Size: " + avl.size() + ". Height: " + avl.height());
-            System.out.println("Min: "+avl.min()+". Max: "+avl.max());
+
+            //Punto a
+            avl.add(1);
+            avl.add(2);
+            avl.add(3);
+            avl.add(4);
+            avl.add(5);
+            avl.add(6);
+            avl.add(7);
+            avl.add(15);
+            avl.add(14);
+            avl.add(13);
+            avl.add(12);
+            avl.add(11);
+            avl.add(10);
+            avl.add(9);
+            avl.add(8);
+
+            //Punto 5.b
+            System.out.println(avl.toString());
+            //Punto 5.c
+            System.out.println("MIN and MAX");
+            System.out.println("Min: "+avl.min()+"\nMax: "+avl.max());
+            System.out.println("\nIs it Balanced? "+avl.isBalanced());
+            System.out.println("");
+            //Punto 5.g
+            avl.remove(11);
+            avl.remove(13);
+            avl.remove(15);
+
+
+            //Punto 5.h
+            System.out.println(avl.toString());
+            //Punto 5.i
+            System.out.println("Is it Balanced? "+avl.isBalanced());
+
+
+
         }catch (TreeException ex){
             throw new RuntimeException(ex);
         }
     }
+
+
+
 }
